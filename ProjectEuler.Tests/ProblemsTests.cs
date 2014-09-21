09d244ef32ea15e251f7720cf7b64031173eb14e
Alexander Kapelyukhovskiy <alkapa2005@gmail.com>
2014-09-21 17:28:44 +0300

    First task commit

---
+++ ProjectEuler.Tests/ProblemsTests.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+using NUnit.Framework;
+
+namespace ProjectEuler.Tests
+{
+    [TestFixture]
+    public class ProblemsTests
+    {
+        [Test]
+        public void Problem0001Test()
+        {
+            Trace.WriteLine(Problem0001.Solution());//233168
+        }
+    }
+}
---
+++ ProjectEuler/Problem0001.cs
@@ -0,0 +1,23 @@
+﻿namespace ProjectEuler
+{
+    public class Problem0001
+    {
+        public static long Solution()
+        {
+            return GetSumOfThreeAndFiveMultipliers(1000);
+        }
+
+        public static long GetSumOfThreeAndFiveMultipliers(long max)
+        {
+            long result = 0;
+            for (long i = 3; i < max; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    result += i;
+                }
+            }
+            return result;
+        }
+    }
+}
