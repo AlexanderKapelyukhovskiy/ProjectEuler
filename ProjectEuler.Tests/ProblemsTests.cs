270ae1cded2d494e7e9ec38b6110155cc8f35571
Alexander Kapelyukhovskiy <alkapa2005@gmail.com>
2014-09-21 19:04:07 +0300

    Problem 2

--- ProjectEuler.Tests/ProblemsTests.cs
+++ ProjectEuler.Tests/ProblemsTests.cs
@@ -11,5 +11,29 @@ namespace ProjectEuler.Tests
         {
             Trace.WriteLine(Problem0001.Solution());//233168
         }
+
+        [Test]
+        public void Problem0002ListTest()
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                long s;
+                long f = Problem0002.Fib(i, out s);
+
+                Trace.WriteLine(string.Format("{0} - {1} - {2}", i, f, s));
+            }
+        }
+
+        [Test]
+        public void Problem0002Test()
+        {
+
+            int n = 2000000;
+            long s;
+            long f = Problem0002.Fib(n, out s);
+
+            Trace.WriteLine(string.Format("{0} - {1} - {2}", n, f, s));
+            
+        }
     }
 }
---
+++ ProjectEuler/Problem0002.cs
@@ -0,0 +1,40 @@
+﻿namespace ProjectEuler
+{
+    public class Problem0002
+    {
+        public long Solution()
+        {
+            return 0;
+        }
+
+        public static long Fib(int n, out long s, long limit = 4000000)
+        {
+            s = 0;
+            if (n == 1)
+                return 1;
+            s = 2;
+            if (n == 2)
+                return 2;
+
+            long sum = 2;
+            long prev1 = 1;
+            long prev2 = 2;
+            long l = 3;
+            for (int i = 3; i <= n; i++)
+            {
+                long t = prev1 + prev2;
+                if (t > limit)
+                    break;
+
+                l = t;
+                if (l % 2 == 0)
+                    sum += l;
+
+                prev1 = prev2;
+                prev2 = l;
+            }
+            s = sum;
+            return l;
+        }
+    }
+}
